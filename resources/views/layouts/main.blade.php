f01a0ffe03d1b7ec0de1ba4ad80b6d6c3c5ef604
fadillatika <dillatika11@gmail.com>
2024-05-03 19:05:07 +0700

    update frontend

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,48 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
+        <!-- Google Fonts -->
+        <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap"
+            rel="stylesheet"
+        />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
+            rel="stylesheet"
+        />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+        />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap"
+        />
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap">
+        <script src="https://unpkg.com/feather-icons"></script>
 
-    <!-- Feather Icons -->
-    <script src="https://unpkg.com/feather-icons"></script>
+        <!-- More Icons -->
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css"
+            integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni"
+            crossorigin="anonymous"
+        />
 
-    <!-- More Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" 
-    integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/custom.css" />
 
-    <link rel="stylesheet" href="css/custom.css">
+        <title>E-bike Monitoring!</title>
+    </head>
 
+    <body>
+        <!-- Navbar start -->
+        @include('partials.navbar')
+        <!-- Hero Section -->
+        <section
+            class="hero"
+            id="home"
+            style="
+                min-height: 100vh;
+                display: flex;
+                align-items: center;
+                background-image: url('/img/bg1.png');
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;
+                position: relative;
+                text-align: center;
+                overflow: hidden;
+            "
+        >
+            @yield('container')
+        </section>
 
-    <title>E-bike Monitoring!</title>
-</head>
+        <script>
+            feather.replace();
+        </script>
 
-<body>
-    <!-- Navbar start -->
-    @include('partials.navbar')
-    <!-- Hero Section -->
-    <section class="hero" id="home" style="min-height: 100vh; 
-    display: flex; align-items: center; background-image: url('/img/bg1.png'); 
-    background-repeat: no-repeat; background-size: cover; 
-    background-position: center; position: relative; 
-    text-align: center; overflow: hidden;">
-        @yield('container')
-    </section>
-
-    <script>
-        feather.replace();
-    </script>
-
-    <!-- Java script -->
-    <script src="js/script.js"></script>
-</body>
-
+        <!-- Java script -->
+        <script src="js/script.js"></script>
+    </body>
 </html>
