06bdd8188cb479eb9854fa3c53f4def4b8a3a975
fadillatika <dillatika11@gmail.com>
2024-06-20 11:39:00 +0700

    updated fetch

--- app/Jobs/FetchThingSpeaksData.php
+++ app/Jobs/FetchThingSpeaksData.php
@@ -31,10 +31,18 @@ class FetchThingSpeaksData implements ShouldQueue
 
         try {
             $apiController = new ApiController();
-            Log::info('Fetching GPS data');
-            $apiController->fetchTSGPS();
+            for ($i = 0; $i < 30; $i++) {
+                Log::info('Fetching GPS data');
+                $apiController->fetchTSGPS();
+                sleep(10);
+            }
+            
+            Log::info('Fetching Battery data');
             $apiController->fetchTSBattery();
+            
+            Log::info('Fetching Lock data');
             $apiController->fetchTSLock();
+            
             Log::info('FetchThingSpeaksData job completed successfully');
         } catch (\Exception $e) {
             Log::error('FetchThingSpeaksData job failed: ' . $e->getMessage());
